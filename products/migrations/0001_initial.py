ccf44d50610a13604167dfda82df53728ddbf2c9
Gwen Bradbury <charmed_89@msn.com>
2021-01-19 12:59:24 +0000

    Add search functionality

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='Products',
+            name='Product',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('sku', models.CharField(blank=True, max_length=254, null=True)),
